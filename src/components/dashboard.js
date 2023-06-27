5018397950205e19c65e127216b1c3c59d962535
Ernor1 <honorerukundo74@gmail.com>
2023-06-27 04:15:18 +0200

    Your commit message here

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,7 +1,8 @@
 import { Fragment, useState } from 'react'
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, useNavigate } from 'react-router-dom';
 import { Dialog, Transition } from '@headlessui/react'
+import axios from 'axios';
 
 import {
   Bars3Icon,
@@ -20,18 +21,32 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-
-
 export default function Dashboard() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-  
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-  const [products, setProducts] = useState([
-    { name: 'Computer', title: 'Technology', email: '5', role: '3000', addedToCart: false },
-    // More products...
-    { name: 'Matress', title: 'Dorm', email: '5', role: '3000', addedToCart: false },
-    { name: 'Soap', title: 'Cleanliness', email: '5', role: '3000', addedToCart: false },
-  ]);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      // Token not found, redirect to login
+      navigate('/login');
+    } else {
+      // Token found, fetch products
+      axios.get('http://localhost:8000/api/product/all', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then(response => response.data)
+        .then(data => setProducts(data))
+        .catch(error => console.error(error));
+    }
+  }, [navigate]);
+
+
+  console.log(products);
 
   const handleAddToCart = (index) => {
     setProducts((prevProducts) => {
@@ -45,14 +60,6 @@ export default function Dashboard() {
 
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
       <div>
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
@@ -205,84 +212,78 @@ export default function Dashboard() {
 
 
 
-            
-          <div className="px-4 sm:px-6 lg:px-8">
-      <div className="px-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          <h1 className="text-base font-semibold leading-6 text-gray-900">Products</h1>
-          <p className="mt-2 text-sm text-gray-700">
-           List of all Products 
-          </p>
-        </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-        <Link
-          to="/cart"
-          className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          View Cart
-        </Link>
-        </div>
-      </div>
-      <div className="-mx-4 mt-8 sm:-mx-0">
-        <table className="min-w-full divide-y divide-gray-300">
-          <thead>
-            <tr>
-              <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                Name
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Type
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-              >
-                Quantity
-              </th>
-              <th
-                scope="col"
-                className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
-              >
-                Price
-              </th>
-             
-              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                <span className="sr-only">Add To Cart</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200 bg-white">
-            {products.map((product, index) => (
-              <tr key={product.email}>
-                <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">{product.name}</td>
-                <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{product.title}</td>
-                <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{product.email}</td>    
-                <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{product.role}</td>        
-                <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-              <button
-                type="button"
-                onClick={() => handleAddToCart(index)}
-                className={classNames(
-                  'text-indigo-600 hover:text-indigo-900',
-                  product.addedToCart ? 'text-green-500 border-green-500' : 'border-green-500'
-                )}
-              >
-                {product.addedToCart ? 'Added to Cart' : 'Add To Cart'}
-                <span className="sr-only">, {product.name}</span>
-              </button>
-            </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-      
-    </div>
+
+            <div className="px-4 sm:px-6 lg:px-8">
+              <div className="px-4 sm:px-6 lg:px-8">
+                <div className="sm:flex sm:items-center">
+                  <div className="sm:flex-auto">
+                    <h1 className="text-base font-semibold leading-6 text-gray-900">Products</h1>
+                    <p className="mt-2 text-sm text-gray-700">
+                      List of all Products
+                    </p>
+                  </div>
+                  <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <Link
+                      to="/cart"
+                      className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      View Cart
+                    </Link>
+                  </div>
+                </div>
+                <div className="-mx-4 mt-8 sm:-mx-0">
+                  <table className="min-w-full divide-y divide-gray-300">
+                    <thead>
+                      <tr>
+                        <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                          Name
+                        </th>
+                        <th
+                          scope="col"
+                          className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                        >
+                          Type
+                        </th>
+
+                        <th
+                          scope="col"
+                          className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                        >
+                          Price
+                        </th>
+
+                        <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                          <span className="sr-only">Add To Cart</span>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 bg-white">
+                      {products.map((product, index) => (
+                        <tr key={product.email}>
+                          <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-0">{product.name}</td>
+                          <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{product.type}</td>
+                          <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{product.price}</td>
+                          <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                            <button
+                              type="button"
+                              onClick={() => handleAddToCart(index)}
+                              className={classNames(
+                                'text-indigo-600 hover:text-indigo-900',
+                                product.addedToCart ? 'text-green-500 border-green-500' : 'border-green-500'
+                              )}
+                            >
+                              {product.addedToCart ? 'Added to Cart' : 'Add To Cart'}
+                              <span className="sr-only">, {product.name}</span>
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+
+            </div>
 
 
 
--- src/components/login.js
+++ src/components/login.js
@@ -9,147 +9,137 @@ export default function Login() {
 
 
 
-    const navigate = useNavigate();
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-  
-      try {
-        console.log(username, password);
-        const response = await axios.post('http://localhost:8000/api/auth/signin', {
-          username,
-          password,
-        });
-  
-        if (response.status === 200) {
-          // Login successful
-          // Redirect to the dashboard or any other page
-     console.log(response.data)
-          const token = response.data.token;
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-          localStorage.setItem('token', token);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-          navigate('/dashboard');
-        } else {
-          // Login failed
-          setErrorMessage(response.data.message);
-        }
-      } catch (error) {
-        // Handle any errors that occurred during the API request
-        console.error(error);
-        setErrorMessage('An error occurred during login');
+    try {
+      console.log(username, password);
+      const response = await axios.post('http://localhost:8000/api/auth/signin', {
+        username,
+        password,
+      });
+
+      if (response.status === 200) {
+        // Login successful
+        // Redirect to the dashboard or any other page
+        console.log(response.data)
+        const token = response.data.token;
+
+        localStorage.setItem('token', token);
+
+        navigate('/dashboard');
+      } else {
+        // Login failed
+        setErrorMessage(response.data.message);
       }
-    };
+    } catch (error) {
+      // Handle any errors that occurred during the API request
+      console.error(error);
+      setErrorMessage('An error occurred during login');
+    }
+  };
 
-    return (
-      <>
-        {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
-        
-        <div className="flex flex-1 min-h-[100vh]">
-          <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-            <div className="mx-auto w-full max-w-sm lg:w-96">
+  return (
+    <>
+      <div className="flex flex-1 min-h-[100vh]">
+        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+              <img
+                className="h-10 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company"
+              />
+              <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign in to your account
+              </h2>
+              <p className="mt-2 text-sm leading-6 text-gray-500">
+                Don't have an account?
+                <a href="/signup" className="font-semibold text-indigo-600 hover:text-indigo-500 ml-4">
+                  Sign up
+                </a>
+              </p>
+            </div>
+
+            <div className="mt-10">
               <div>
-                <img
-                  className="h-10 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt="Your Company"
-                />
-                <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                  Sign in to your account
-                </h2>
-                <p className="mt-2 text-sm leading-6 text-gray-500">
-                  Don't have an account?
-                  <a href="/signup" className="font-semibold text-indigo-600 hover:text-indigo-500 ml-4">
-                    Sign up
-                  </a>
-                </p>
-              </div>
-  
-              <div className="mt-10">
-                <div>
-                  <form onSubmit={handleSubmit} action="#" method="POST" className="space-y-6">
-                    <div>
-                      <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                        Email
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          id="email"
-                          name="email"
-                          type="text"
-                          required
-                          className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          onChange={(event)=>{
-                            setUsername(event.target.value);
-                          }}
-                        />
-                      </div>
+                <form onSubmit={handleSubmit} action="#" method="POST" className="space-y-6">
+                  <div>
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                      Email
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        name="email"
+                        type="text"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        onChange={(event) => {
+                          setUsername(event.target.value);
+                        }}
+                      />
                     </div>
-  
-                    <div>
-                      <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                        Password
-                      </label>
-                      <div className="mt-2">
-                        <input
-                          id="password"
-                          name="password"
-                          type="password"
-                          required
-                          className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          onChange={(event)=>{
-                            setPassword(event.target.value);
-                          }}
-                        />
-                      </div>
+                  </div>
+
+                  <div>
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      Password
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        onChange={(event) => {
+                          setPassword(event.target.value);
+                        }}
+                      />
                     </div>
-  
-                    <div className="flex items-center justify-between">
-                    
-                      <div className="text-sm leading-6">
-                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                          Forgot password?
-                        </a>
-                      </div>
+                  </div>
+
+                  <div className="flex items-center justify-between">
+
+                    <div className="text-sm leading-6">
+                      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                        Forgot password?
+                      </a>
                     </div>
-  
-                    <div>
-                      <button
-                        type="submit"
-                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                        Sign in
-                      </button>
-                    </div>
-                  </form>
-                </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
-          <div className="relative hidden w-0 flex-1 lg:block">
-            <img
-              className="absolute inset-0 h-full w-full object-cover"
-              src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-              alt=""
-            />
-          </div>
-
-         
-          
         </div>
-        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        <div className="relative hidden w-0 flex-1 lg:block">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            alt=""
+          />
+        </div>
 
-      </>
-    )
-  }
-  
+
+
+      </div>
+      {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+
+    </>
+  )
+}
